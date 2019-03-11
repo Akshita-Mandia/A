500bb30123b4c247fdd5367e8c54a6bb110ec026
Akshita-Mandia <41584153+Akshita-Mandia@users.noreply.github.com>
2019-03-11 23:16:01 +0530

    Assignment-2

---
+++ Assignment-2/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const Home = () => {
+  return(
+    <div>
+      <div className='container center'>
+        <h4>Home Page</h4>
+        <p> This my home page. </p>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
